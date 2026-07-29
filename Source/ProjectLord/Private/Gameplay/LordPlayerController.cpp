7454a3db5a0370a8e61c5f7c74de5e01bc48dc93
Skyler Manzanares <skymanzanares@hotmail.com>
2026-07-29 11:04:01 -0700

    Added click-handling input and processing on player controller (BP, no asset refs in c++) and added a dirty text printout of selection to the dev overlay.
    Resolves #10

--- Source/ProjectLord/Private/Gameplay/LordPlayerController.cpp
+++ Source/ProjectLord/Private/Gameplay/LordPlayerController.cpp
@@ -9,6 +9,8 @@
 
 ALordPlayerController::ALordPlayerController()
 {
+	bShowMouseCursor = true;
+	bEnableClickEvents = true;
 }
 
 void ALordPlayerController::BeginPlay()
@@ -40,4 +42,52 @@ EUnitTeam ALordPlayerController::GetTeam() const
 	}
 
 	return EUnitTeam::Player1;
+}
+
+void ALordPlayerController::SetSelection(USelectionComponent* InSelection)
+{
+	ClearSelection(); // To issue deselect callbacks
+	Selection = InSelection->Select();
+}
+
+void ALordPlayerController::ClearSelection()
+{
+	if (HasSelection())
+	{
+		if (Selection->SelectedComponent.IsValid())
+		{
+			Selection->SelectedComponent->Deselect();
+		}
+	}
+	Selection = NullOpt;
+}
+
+void ALordPlayerController::OnMouseClick(bool bRightButton)
+{
+	if (!bRightButton)
+	{
+		FHitResult HitResult;
+		if (GetHitResultUnderCursorByChannel(UEngineTypes::ConvertToTraceType(ECC_Pawn), true, HitResult)
+			&& IsValid(HitResult.GetActor())
+			&& CanSelect(HitResult.GetActor())
+			)
+		{
+			// CanSelect was called already, so should have a selection component
+			USelectionComponent* ClickedSelection = HitResult.GetActor()->GetComponentByClass<USelectionComponent>();
+			if (ensure(ClickedSelection))
+			{
+				SetSelection(ClickedSelection);
+			}
+
+		}
+		else
+		{
+			ClearSelection();
+		}
+	}
+}
+
+bool ALordPlayerController::CanSelect(const AActor* ClickedActor) const
+{
+	return IsValid(ClickedActor->GetComponentByClass<USelectionComponent>());
 }
--- Source/ProjectLord/Public/Gameplay/LordPlayerController.h
+++ Source/ProjectLord/Public/Gameplay/LordPlayerController.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 
 #include "GameFramework/PlayerController.h"
+#include "Gameplay/SelectionComponent.h" // For FSelectionData in optional
 #include "Gameplay/Combat/CombatTypes.h"
 
 #include "LordPlayerController.generated.h"
@@ -28,8 +29,27 @@ public:
     UFUNCTION(BlueprintPure)
     EUnitTeam GetTeam() const;
 
+    UFUNCTION(BlueprintPure, Category = "Selection")
+    bool HasSelection() const { return Selection.IsSet(); };
+
+    UFUNCTION(BlueprintPure, Category = "Selection")
+    FSelectionData GetSelection() const { if (ensure(HasSelection())) return Selection.GetValue(); return {}; };
+
+    UFUNCTION(BlueprintCallable, Category = "Selection")
+    void SetSelection(USelectionComponent* Selection);
+
+    UFUNCTION(BlueprintCallable, Category = "Selection")
+    void ClearSelection();
+
 protected:
+
+    UFUNCTION(BlueprintCallable)
+    void OnMouseClick(bool bRightButton);
 
     UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Defaults")
     TObjectPtr<UInputMappingContext> StartingInputContext;
+
+    TOptional<FSelectionData> Selection;
+
+    bool CanSelect(const AActor* ClickedActor) const;
 };
