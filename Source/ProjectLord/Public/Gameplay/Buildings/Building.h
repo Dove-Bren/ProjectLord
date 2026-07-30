3df8bd4b02010077ce40ff5d9f1c0b1cb37fecb5
Skyler Manzanares <skymanzanares@hotmail.com>
2026-07-29 23:41:11 -0700

    Expand buildings to have a list of meshes that changes depending on the building level.

--- Source/ProjectLord/Public/Gameplay/Buildings/Building.h
+++ Source/ProjectLord/Public/Gameplay/Buildings/Building.h
@@ -16,6 +16,8 @@ class UAbilitySystemComponent;
 class UCombatComponent;
 class ABuildingController;
 class USelectionComponent;
+class UStaticMesh;
+class UStaticMeshComponent;
 
 UCLASS(Blueprintable)
 class PROJECTLORD_API ABuilding : public APawn
@@ -70,16 +72,19 @@ public:
     UFUNCTION(BlueprintPure)
     UCombatAttributeSet* GetCombatAttributeSet() const { return CombatAttributeSet; }
 
+    UFUNCTION(BlueprintPure)
+    UStaticMesh* GetBuildingMesh() const;
+
 protected:
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Building|Definition")
+    UStaticMeshComponent* BuildingMesh;
 
     UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Building|Definition")
     TObjectPtr<UBuildingType> BuildingType;
 
     UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Building|Definition")
     FText BuildingCustomName;
-
-    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Building|Definition")
-    bool bSupportsLevel;
 
     UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Building|Definition")
     bool bSupportsResidents;
@@ -95,6 +100,24 @@ protected:
 
     UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Building|Definition")
     int GoldGeneratedPerDay;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Building|Definition")
+    int MaxLevel;
+
+    // Meshes to match to each level of the building. There should be one entry in here
+    // every 'MaxLevel' -- aka if MaxLevel is 3, there should be 3 entries.
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Building|Definition")
+    TArray<UStaticMesh*> BuildingLevelMeshes;
+
+    // Meshes to cycle through when constructing the building. Mesh will be
+    // selected automatically based on how far along construction is and how many
+    // construction meshes are available.
+    // For example, if there are 3 meshes, the first will be shown from 0-33%,
+    // the second from 33% to 66%, and the third from 66% to 100%. At that point,
+    // the first level mesh will be shown.
+    // There should always be at least 1 of these.
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Building|Definition")
+    TArray<UStaticMesh*> BuildingConstructionMeshes;
 
     UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Building|Definition")
     FVector BuildingEntranceOffset;
@@ -120,6 +143,9 @@ protected:
     UPROPERTY(VisibleInstanceOnly, BlueprintReadWrite, Category = "Building|Contents")
     int BuildingGold;
 
+    UPROPERTY(VisibleInstanceOnly, BlueprintReadWrite, Category = "Building")
+    int BuildingLevel;
+
     // Creatures that consider this building their home
     UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category = "Building|Contents")
     TArray<ACreature*> Residents;
@@ -137,10 +163,15 @@ protected:
 
     virtual void SetupBaseAttributes();
 
+    int GetBuildingHealth() const;
+    int GetBuildingMaxHealth() const;
+
     UFUNCTION()
     void HandleDeath();
 
 public:
     virtual void BeginPlay() override;
     virtual void EndPlay(EEndPlayReason::Type Reason) override;
+    virtual void OnConstruction(const FTransform& Transform) override;
+    virtual void Tick(float DeltaSeconds) override;
 };
